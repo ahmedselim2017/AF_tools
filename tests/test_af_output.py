3ef9fe43b565c6cce5a62bb50b45fbe372f1ced4
ahmedselim2017 <ahmedselimuzum@gmail.com>
2024-05-25 16:03:08 +0300

    Changed test file structure

--- tests/test_af_output.py
+++ tests/test_af_output.py
@@ -3,12 +3,6 @@ import pytest
 from af_tools.afparser import AFParser
 
 
-def test_af3_rmsd() -> None:
-    af3output = AFParser("./tests/data/af3_recursive",
-                         process_number=12).get_output()
-    af3output.calculate_rmsds(0, 0)
-
-
 def test_invalid_path() -> None:
     with pytest.raises(Exception) as e_info:
         AFParser("./tests/data/wrongpath").get_output()
@@ -28,75 +22,13 @@ def test_af3_pred_detection() -> None:
     assert af3output.predictions[0].models != None
 
 
-def test_colabfold_pred_plddt() -> None:
-    colabfold_output = AFParser("./tests/data/colabfold").get_output()
-    fig = colabfold_output.plot_all_plddts()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_colabfold_multimer_pred_plddt() -> None:
-    colabfold_output = AFParser("./tests/data/colabfold_multimer").get_output()
-    fig = colabfold_output.plot_all_plddts()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_af3_pred_plddt() -> None:
-    af3output = AFParser("./tests/data/af3").get_output()
-    fig = af3output.plot_all_plddts()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_af3_multimer_pred_plddt() -> None:
-    af3output = AFParser("./tests/data/af3_multimer").get_output()
-    fig = af3output.plot_all_plddts()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_af3_pred_pae() -> None:
-    af3output = AFParser("./tests/data/af3").get_output()
-    fig = af3output.plot_all_plddts()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_af3_multimer_pred_pae() -> None:
-    af3output = AFParser("./tests/data/af3_multimer").get_output()
-    fig = af3output.plot_all_paes()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_colabfold_multimer_pred_pae() -> None:
-    af2output = AFParser("./tests/data/colabfold_multimer").get_output()
-    fig = af2output.plot_all_paes()[0]
-
-    # fig.show()
-    # input()
-
-
-def test_colabfold_hist() -> None:
-    af2output = AFParser("./tests/data/colabfold_recursive",
+def test_af2_rmsd() -> None:
+    af3output = AFParser("./tests/data/colabfold_recursive",
                          process_number=12).get_output()
-    fig = af2output.plot_plddt_hist()
-
-    # fig.show()
-    # input()
+    af3output.calculate_rmsds(0, 0)
 
 
-def test_af3_hist() -> None:
+def test_af3_rmsd() -> None:
     af3output = AFParser("./tests/data/af3_recursive",
                          process_number=12).get_output()
-    fig = af3output.plot_plddt_hist()
-
-    # fig.show()
-    # input()
+    af3output.calculate_rmsds(0, 0)
---
+++ tests/test_plotter.py
@@ -0,0 +1,77 @@
+import pytest
+
+from af_tools.afparser import AFParser
+
+
+def test_colabfold_pred_plddt() -> None:
+    colabfold_output = AFParser("./tests/data/colabfold").get_output()
+    fig = colabfold_output.plot_all_plddts()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_colabfold_multimer_pred_plddt() -> None:
+    colabfold_output = AFParser("./tests/data/colabfold_multimer").get_output()
+    fig = colabfold_output.plot_all_plddts()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_af3_pred_plddt() -> None:
+    af3output = AFParser("./tests/data/af3").get_output()
+    fig = af3output.plot_all_plddts()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_af3_multimer_pred_plddt() -> None:
+    af3output = AFParser("./tests/data/af3_multimer").get_output()
+    fig = af3output.plot_all_plddts()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_af3_pred_pae() -> None:
+    af3output = AFParser("./tests/data/af3").get_output()
+    fig = af3output.plot_all_plddts()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_af3_multimer_pred_pae() -> None:
+    af3output = AFParser("./tests/data/af3_multimer").get_output()
+    fig = af3output.plot_all_paes()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_colabfold_multimer_pred_pae() -> None:
+    af2output = AFParser("./tests/data/colabfold_multimer").get_output()
+    fig = af2output.plot_all_paes()[0]
+
+    # fig.show()
+    # input()
+
+
+def test_colabfold_hist() -> None:
+    af2output = AFParser("./tests/data/colabfold_recursive",
+                         process_number=12).get_output()
+    fig = af2output.plot_plddt_hist()
+
+    # fig.show()
+    # input()
+
+
+def test_af3_hist() -> None:
+    af3output = AFParser("./tests/data/af3_recursive",
+                         process_number=12).get_output()
+    fig = af3output.plot_plddt_hist()
+
+    # fig.show()
+    # input()
