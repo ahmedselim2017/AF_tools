f2f8f00db0646aa6aebaa4de6d41d182aa3f9a23
ahmedselim2017 <ahmedselimuzum@gmail.com>
2024-06-10 01:47:02 +0300

    Some small improvements

--- af_tools/utils.py
+++ af_tools/utils.py
@@ -15,13 +15,13 @@ from numpy.typing import NDArray
 from af_tools.output_types import AF2Model, AF2Prediction, AF3Prediction, AFPrediction
 
 
-def load_structure(path: str) -> Structure:
-    if path.split(".")[-1] == "cif":
+def load_structure(path: Path) -> Structure:
+    if path.suffix == ".cif":
         parser = MMCIFParser()
-    elif path.split(".")[-1] == "pdb":
+    elif path.suffix == "pdb":
         parser = PDBParser()
     else:
-        raise Exception(f"Unknwon model file types:{path}")
+        raise TypeError(f"Unknwon model file types:{path}")
     return parser.get_structure(path, path)
 
 
@@ -34,9 +34,13 @@ def calculate_rmsd(target_model_path: Any,
 
 
 @calculate_rmsd.register
-def _(target_model_path: str, ref_model_path: str) -> float | NDArray:
-    ref_model = load_structure(ref_model_path)
-    target_model = load_structure(target_model_path)
+def _(target_model: Path | Structure,
+      ref_model: Path | Structure) -> float | NDArray:
+
+    if isinstance(target_model, Path):
+        target_model = load_structure(target_model)
+    if isinstance(ref_model, Path):
+        ref_model = load_structure(ref_model)
 
     ref_CAs: list[Atom] = []
     target_CAs: list[Atom] = []
@@ -63,44 +67,6 @@ def _(target_model_path: list[str], ref_model_path: str) -> float | NDArray:
     return rmsds
 
 
-def worker_calculate_rmsd2(ref_model: Structure | str, target_model_path: str,
-                           index) -> tuple:
-
-    target_model_structure = load_structure(target_model_path)
-
-    if isinstance(ref_model, str):
-        ref_model_structure = load_structure(ref_model)
-    else:
-        ref_model_structure = ref_model
-
-    if ref_model_structure == target_model_structure:
-        return (index, 0)
-
-    m1_cas: list[Atom] = []
-    m2_cas: list[Atom] = []
-    for m1_res, m2_res in zip(ref_model_structure.get_residues(),
-                              target_model_structure.get_residues()):
-        assert m1_res.resname == m2_res.resname
-        assert m1_res.id == m2_res.id
-
-        m1_cas.append(m1_res["CA"])
-        m2_cas.append(m2_res["CA"])
-
-    sup = Superimposer()
-    sup.set_atoms(m1_cas, m2_cas)
-
-    assert isinstance(sup.rms, float)
-
-    return (index, sup.rms)
-
-
-def worker_wrapper_calculate_rmsd(args: tuple) -> tuple:
-    ref_model_structure, target_model_path, index = args
-    return worker_calculate_rmsd(ref_model=ref_model_structure,
-                                 target_model_path=target_model_path,
-                                 index=index)
-
-
 def worker_af2output_get_pred(path: Path) -> Sequence[AF2Prediction]:
     from af_tools.data_types.af2output import AF2Output
     af2output = AF2Output(path=path, process_number=1)
@@ -112,27 +78,3 @@ def worker_af3output_get_pred(path: Path) -> Sequence[AF3Prediction]:
     from af_tools.data_types.af3output import AF3Output
     af3output = AF3Output(path=path, process_number=1)
     return af3output.predictions
-
-
-def worker_pred_ref_rmsd(pred: AFPrediction,
-                         index: int) -> tuple[NDArray, int]:
-    rmsds = np.full(len(pred.models), np.nan, dtype=float)
-    ref_model = load_structure(str(pred.reference_path))
-
-    for i, model in enumerate(pred.models):
-        if isinstance(model, AF2Model):
-            target_model_path = model.relaxed_pdb_path
-        else:
-            target_model_path = model.model_path
-
-        rmsds[i] = worker_calculate_rmsd(
-            ref_model=ref_model,
-            target_model_path=str(target_model_path),
-            index=i)
-
-    return (rmsds, index)
-
-
-def worker_wrapper_pred_ref_rmsd(args) -> tuple[NDArray, int]:
-    pred, index = args
-    return worker_pred_ref_rmsd(pred, index)
